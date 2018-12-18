98acf699535f1a260d469827785a7c75e26ab7ab
htata31 <htata31@gmail.com>
2018-12-18 08:47:51 -0600

    Changes done in server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.get('/', function(req, res) {
-    res.render('../LoginPage.html');
+    res.render('LoginPage.html');
 })
 
 app.post('/enroll', function (req, res) {
